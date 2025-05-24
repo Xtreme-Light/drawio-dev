59884f14692f32e1a86ca328636adc67122e2f07
Xtreme_Light <whateverxl@outlook.com>
2025-05-25 01:08:24 +0800

    feature: 1、删除后端文件；2、给出默认配置

--- src/main/webapp/js/PreConfig.js
+++ src/main/webapp/js/PreConfig.js
@@ -10,5 +10,60 @@ window.DRAWIO_BASE_URL = null; // Replace with path to base of deployment, e.g. 
 window.DRAWIO_VIEWER_URL = null; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
 window.DRAWIO_LIGHTBOX_URL = null; // Replace with your lightbox URL, eg. https://www.example.com
 window.DRAW_MATH_URL = 'math/es5';
-window.DRAWIO_CONFIG = null; // Replace with your custom draw.io configurations. For more details, https://www.drawio.com/doc/faq/configure-diagram-editor
+// Replace with your custom draw.io configurations. For more details, https://www.drawio.com/doc/faq/configure-diagram-editor
+window.DRAWIO_CONFIG = {
+    "defaultLibraries": "basic;flowchart;体系",
+    "enabledLibraries": [
+        "basic",
+        "flowchart",
+        "体系"
+    ],
+    "defaultCustomLibraries": [
+        "体系"
+    ],
+    "appendCustomLibraries": true,
+    "libraries": [
+        {
+            "title": {
+                "main": "业务架构"
+            },
+            "entries": [
+                {
+                    "id": "体系",
+                    "title": {
+                        "main": "业务体系",
+                        "zh": "业务体系"
+                    },
+                    "desc": {
+                        "main": "业务架构下的第一层级，业务体系",
+                        "zh": "业务架构下的第一层级，业务体系"
+                    },
+                    "libs": [
+                        {
+                            "title": {
+                                "main": "业务体系",
+                                "zh": "业务体系"
+                            },
+                            "data": [
+                                {
+                                    "xml": "<mxGraphModel><root><mxCell id=\"0\"/><mxCell id=\"1\" parent=\"0\"/><mxCell id=\"2\" value=\"新增业务\" style=\"rounded=1;whiteSpace=wrap;html=1;\" vertex=\"1\" parent=\"1\"><mxGeometry width=\"120\" height=\"60\" as=\"geometry\"/></mxCell></root></mxGraphModel>",
+                                    "w": 120,
+                                    "h": 60,
+                                    "aspect": "fixed",
+                                    "title": "第二个"
+                                },
+                                {
+                                    "xml": "<mxGraphModel><root><mxCell id=\"0\"/><mxCell id=\"1\" parent=\"0\"/><mxCell id=\"2\" value=\"Text\" style=\"text;html=1;align=center;verticalAlign=middle;whiteSpace=wrap;rounded=0;\" vertex=\"1\" parent=\"1\"><mxGeometry width=\"60\" height=\"30\" as=\"geometry\"/></mxCell></root></mxGraphModel>",
+                                    "w": 60,
+                                    "h": 30,
+                                    "title": "第三个"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
 urlParams['sync'] = 'manual';
